f1fd1b2fc82eb704d6e8f7f0b3c1f628c0b25680
Denis Irz <denirz@gmail.com>
2018-12-15 23:54:00 +0300

    удалил Bcolor - которым пользоваться пока явно не буду.

--- do_spaces_utils.py
+++ do_spaces_utils.py
@@ -20,18 +20,6 @@ SECRET_KEY=cp.get('basic','SECRET_KEY')
 REGION = cp.get('basic','REGION')
 BUCKET = cp.get('basic','BUCKET')
 
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 def auth(bucket=BUCKET):
     """
     Должен  возвращать объект Bucket в котором хранятся все объекты
