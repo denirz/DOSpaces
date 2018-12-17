a5b6afe917519e5a5f0c93c772b876b2b4f88825
Denis Irz <denirz@gmail.com>
2018-12-17 12:15:25 +0300

    dofiles - вставил 2 ключа чтобы попробовать как это работает.  но пока ни ничего не делают на самом то деле

--- dofiles.py
+++ dofiles.py
@@ -3,17 +3,59 @@ import argparse
 import  do_spaces_utils
 def readargs():
     parser = argparse.ArgumentParser(description='Digital Ocean Spaces Files')
-    parser.add_argument('-l','--list',action='store_true',dest='listfiles',help="list all files")
-    parser.add_argument('download',nargs=2,help="downloads file into target dir ")
+    parser.add_argument('-l','--list',dest='list', action='store_true', default=False,
+                        help="list all files"
+                        )
+
+    parser.add_argument('-u','--upload',
+                        action='store',
+                        nargs='?',
+                        dest = 'filepath',
+                        help = "Upload file"
+                        )
+    parser.add_argument('-k','--key',
+                        action='store',
+                        nargs='?',
+                        dest='key',
+                        default="-",
+                        help='Remote key file',)
+
+    # subparser = parser.add_subparsers(help="Command to do")
+    # parser_lists=subparser.add_parser('list',help='List files')
+    # parser_upload=subparser.add_parser('upload',help='Upload files')
+    # parser_download=subparser.add_parser('download',help='Upload files')
+
+
+    # parser_lists.add_argument('-l','--list',action='store_true',dest='listfiles',help="list all files")
+    # parser_upload.add_argument('download',action='append',nargs='*',help="downloads <key> file into target <dir> ")
+    # subparser.add_parser('CP')
+    # subparser.add_parser('Download')
+
     res = parser.parse_args()
-    if res.listfiles:
-        res=do_spaces_utils.MyBucket().listfiles()
-        for item in res:
-            print u"{}".format(item)
-        print "-"*10
-        print u"Total {} items".format(len(res))
-        exit(0)
-    print res.download[1]
+    print "RES:{}".format(res)
+
+
+
+    if res.list:
+        print "LISTING FILES and exit"
+
+
+    if res.filepath:
+        print u"uploading file \'{}\' with key {} ".format(res.upload,res.key)
+
+    # res = parser_lists.parse_args()
+    # print res
+    # if res.listfiles:
+    #     res=do_spaces_utils.MyBucket().listfiles()
+    #     for item in res:
+    #         print u"{}".format(item)
+    #     print "-"*10
+    #     print u"Total {} items".format(len(res))
+    #     exit(0)
+    # if res.download:
+    #     print res.download[1]
+    #     print res.download[2]
+    #
 
     # print "Hello"
 
--- tests/test_dofiles.py
+++ tests/test_dofiles.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from unittest import TestCase
 import os
 import sys
@@ -32,6 +33,7 @@ class Testdofile(TestCase):
         cmd=self.cmd
         cmd.append('download')
         cmd.append('/data')
+        cmd.append('/asdasd/data')
         print cmd
         res = subprocess.check_output(cmd)
         print res
@@ -43,3 +45,45 @@ class Testdofile(TestCase):
         res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         self.assertRegexpMatches(res,'optional arguments')
         print res
+
+    def test_list(self):
+        cmd =self.cmd
+        cmd.append('-l')
+        print cmd
+        res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        # self.assertRegexpMatches(res,'optional arguments')
+        print res
+
+
+
+    def test_subparser(self):
+        cmd = self.cmd
+        cmd.append('-u')
+        cmd.append('source')
+        print cmd
+        res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        # self.assertRegexpMatches(res, 'optional arguments')
+        print res
+
+        cmd.append('-k')
+        print cmd
+        res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        # self.assertRegexpMatches(res, 'optional arguments')
+        print res
+
+        cmd.append(u'/asdasd kjhda/рол')
+        print cmd
+        res = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        # self.assertRegexpMatches(res, 'optional arguments')
+        print res
+
+
+        cmd = self.cmd
+        cmd=cmd[:-1]
+        # cmd.append('-u')
+        # cmd.append('source')
+        print cmd
+        res = subprocess.check_output(cmd)
+        # self.assertRegexpMatches(res, 'optional arguments')
+        print res
+
